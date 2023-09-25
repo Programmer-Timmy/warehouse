f9734b9b73fe32591b93010c82bf8efff048016c
Programmer-Timmy <tim.vanderkloet@gmail.com>
2023-09-25 11:23:59 +0200

    added al classes

--- public/index.php
+++ public/index.php
@@ -1,17 +1,19 @@
 <?php
+require_once "../private/autoloader.php";
+//require_once "../static/settings.php";
 
-?>
+$filename = substr($_SERVER['REDIRECT_URL'], 1);
+if ($filename == '') {
+    $filename = 'home';
+    if(!isset($_SESSION['user'])){
+        $filename = 'login';
+    }
+}
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-</head>
-<body>
-
-</body>
-</html>
+if (file_exists($filename . '.php')) {
+    include($filename . '.php');
+} elseif (file_exists($filename . '.html')) {
+    include($filename . '.html');
+} else {
+    include('404.php');
+}
